d132283cfe6d8515f199c258767fe372345b9486
kytaec <vladislavtokovenko4@gmail.com>
2025-05-25 11:20:25 +0300

    Add Task 1 Step 2
    - Add a comment for testing purpose

--- spaceship/app.py
+++ spaceship/app.py
@@ -5,7 +5,7 @@ from starlette.responses import FileResponse
 from spaceship.config import Settings
 from spaceship.routers import api, health
 
-
+# Some comments for testing 
 def make_app(settings: Settings) -> FastAPI:
     app = FastAPI(
         debug=settings.debug,
